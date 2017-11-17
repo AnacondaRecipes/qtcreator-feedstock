36b1131c926967bce18b2f01fa7159d9b3912264
Ray Donnelly <mingw.android@gmail.com>
2017-11-17 12:43:57 +0000

    Add qbs as a separate project

---
+++ recipe/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ ! -f ${PREFIX}/bin/llvm-config ]]; then
+  echo "You need to add a host dep of llvmdev for the Clang Code Model"
+  exit 1
+fi
+
+# Avoid Xcode
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  mkdir xcode
+  pushd xcode
+    cp "${RECIPE_DIR}"/xcrun .
+    cp "${RECIPE_DIR}"/xcodebuild .
+    PATH=${PWD}:${PATH}
+  popd
+fi
+
+# This appears in the "About" dialog, but qmake is not good and I cannot
+# find any way to prevent it getting mangled (-DAnaconda -DBuild ...)
+echo DEFINES += IDE_VERSION_DESCRIPTION=\\\"Anaconda Build ${PKG_BUILDNUM}\\\" >> qtcreator.pro
+qmake -r qtcreator.pro                   \
+      QTC_PREFIX=/                       \
+      QBS_INSTALL_PREFIX=/               \
+      LLVM_INSTALL_DIR=${PREFIX}         \
+      QMAKE_CC=${CC}                     \
+      QMAKE_CXX=${CXX}                   \
+      QMAKE_LINK=${CXX}                  \
+      QMAKE_LINK_C=${CC}                 \
+      QMAKE_CXXFLAGS="${CXXFLAGS}"       \
+      QMAKE_LFLAGS_RELEASE="${LDFLAGS}"
+make -j${CPU_COUNT}
